d25db7d38de3475b911a3c0588acc74dbb29d680
jzkool <plopplopshitshit@gmail.com>
2026-01-15 17:54:03 -0500

    Update __init__.py

--- Gaia's Mirror/Utils/__init__.py
+++ Gaia's Mirror/Utils/__init__.py
@@ -1,1 +1,14 @@
+# gaia_mirror/utils/__init__.py
 
+# This file marks the utils directory as a Python sub-package.
+
+# It can be used to export specific utility functions or classes directly
+# from the 'utils' package, e.g., 'from gaia_mirror.utils import DataValidator'.
+
+from .data_validation import DataValidator, DataValidationError
+from .causal_tracing import CausalTracer, CausalTracingError, CausalPathway
+from .quantum_hpc_connector import QuantumHPCManager, HPCConnector, QuantumConnector, ComputeTask, ComputeOffloadError
+
+# Optionally, a package-level logger for utils
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
